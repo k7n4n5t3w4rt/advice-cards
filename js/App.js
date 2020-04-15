9147bca8740ab5c1e79137a54d8f4f2f1ee4c919
Kynan Stewart <2020@mx.kynan.net.au>
2020-04-15 20:31:15 +1000

    Images

--- js/App.js
+++ js/App.js
@@ -1,13 +1,44 @@
-import { h, render } from '../web_modules/preact.js'
-import { useState } from '../web_modules/preact/hooks.js'
+import { h, render } from '../web_modules/preact.js';
+import { useState } from '../web_modules/preact/hooks.js';
+import { css } from '../web_modules/emotion.js';
 import Counter from './Counter.js';
-import htm from '../web_modules/htm.js'
-const html = htm.bind(h)
+import htm from '../web_modules/htm.js';
+const html = htm.bind(h);
 
+const imageContainer = css`
+	background-color: gold;
+	min-height: 100vh;
+	min-width: 500vw;
+`
+const box = css`
+	height: 100vh;
+	width: 100vw;
+	display: inline-block;
+`
+const adviceImage = css`
+	height: 100%;
+	width: 100%;
+	object-fit: cover;
+`
 const App = props => {
 	return html`
-    <h1>Look Ma! No script tags, no build step</h1>
-    <${Counter} count=0 />
+	<div className=${imageContainer}>
+		<div className="${box}">
+			<img className="${adviceImage}" src="/img/001_letmeknow.png" title="Let me know" />
+		</div>
+		<div class="${box}">
+			<img className="${adviceImage}" src="/img/002_iadvise.png" title="I advise" />
+		</div>
+		<div class="${box}">
+			<img className="${adviceImage}" src="/img/003_decidetogether.png" title="We decide together" />
+		</div>
+		<div class="${box}">
+			<img className="${adviceImage}" src="/img/004_decidewithadvice.png" title="I decide with advice" />
+		</div>
+		<div class="${box}">
+			<img className="${adviceImage}" src="/img/005_idecide.png" title="I decide" />
+		</div>
+	</div>
   `
 }
 
