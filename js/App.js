eb56ead4d5856ede61c4513b3dbd2d4520713546
Kynan Stewart <2020@mx.kynan.net.au>
2020-04-15 20:56:30 +1000

    Credits.

--- js/App.js
+++ js/App.js
@@ -8,12 +8,18 @@ const html = htm.bind(h);
 const imageContainer = css`
 	background-color: gold;
 	min-height: 100vh;
-	min-width: 500vw;
+	min-width: 600vw;
 `
 const box = css`
 	height: 100vh;
 	width: 100vw;
 	display: inline-block;
+`
+const text = css`
+	padding: 10px;
+	font-family: sans-serif;
+	text-align: center;
+	font-size: 2em;
 `
 const adviceImage = css`
 	height: 100%;
@@ -26,17 +32,20 @@ const App = props => {
 		<div className="${box}">
 			<img className="${adviceImage}" src="/img/001_letmeknow.png" title="Let me know" />
 		</div>
-		<div class="${box}">
+		<div className="${box}">
 			<img className="${adviceImage}" src="/img/002_iadvise.png" title="I advise" />
 		</div>
-		<div class="${box}">
+		<div className="${box}">
 			<img className="${adviceImage}" src="/img/003_decidetogether.png" title="We decide together" />
 		</div>
-		<div class="${box}">
+		<div className="${box}">
 			<img className="${adviceImage}" src="/img/004_decidewithadvice.png" title="I decide with advice" />
 		</div>
-		<div class="${box}">
+		<div className="${box}">
 			<img className="${adviceImage}" src="/img/005_idecide.png" title="I decide" />
+		</div>
+		<div className="${box}">
+			<p className="${text}">"Advice Cards" are made by <a href="https://agilitrix.com/2016/04/improve-decision-making-advice-cards/">Michael Sahota</a></p>
 		</div>
 	</div>
   `
