f5095e1b6904d1be30ba422ba9acf7b30ae5b8ff
Kynan Stewart <2020@mx.kynan.net.au>
2020-04-16 12:25:59 +1000

    Screenfull

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-import { h, hydrate } from '../web_modules/preact.js'
-import App from './App.js';
-import htm from '../web_modules/htm.js'
-const html = htm.bind(h)
+import { h, hydrate } from "../web_modules/preact.js";
+import App from "./App.js";
+import htm from "../web_modules/htm.js";
+import screenfull from "../web_modules/screenfull.js";
+const html = htm.bind(h);
+
+if (screenfull.isEnabled) {
+	screenfull.request();
+}
 
 hydrate(
 	html`
-    <${App} />
-  `,
-	document.getElementById('main-container')
-)
+		<${App} />
+	`,
+	document.getElementById("main-container")
+);
